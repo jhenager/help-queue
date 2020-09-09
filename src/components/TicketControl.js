3c4504e75356d1298b51cc12f803323e36d47c8f
James Henager <jameschenager@gmail.com>
2020-09-09 14:25:49 -0700

    add ticket form
    Co-authored-by: Taylor Somers <tsomers@yandex.com>

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -12,22 +12,25 @@ class TicketControl extends React.Component {
   }
 
   handleClick = () => {
-    
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
   }
 
   render(){
     let currentlyVisibleState = null;
-    let addTicketButton = null;
+    let buttonText = null;
     if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewTicketForm />
+      buttonText = "Return to Ticket List";
     } else {
       currentlyVisibleState = <TicketList />
-      addTicketButton = <button onClick={this.handClick}>Add Ticket</button>
+      buttonText = "Add Ticket";
     }
     return (
       <React.Fragment>
         {currentlyVisibleState}
-        {addTicketButton}
+        <button onClick={this.handleClick}>{buttonText}</button>
       </React.Fragment>
     );
   }
