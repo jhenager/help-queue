dd99c3b7262edaf26da149f6275bcb527fad15c2
James Henager <jameschenager@gmail.com>
2020-09-10 15:57:27 -0700

    finished example

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { v4 } from 'uuid';
+import PropTypes from "prop-types";
 
 function NewTicketForm(props){
 
-  
+  function handleNewTicketFormSubmission(event) {
+    event.preventDefault();
+    props.onNewTicketCreation({names: event.target.names.value, location: event.target.location.value, issue: event.target.issue.value, id: v4()});
+  }
 
   return (
     <React.Fragment>
@@ -24,11 +28,11 @@ function NewTicketForm(props){
     </React.Fragment>
   );
 }
-function handleNewTicketFormSubmission(event) {
-  event.preventDefault();
-  console.log(event.target.names.value);
-  console.log(event.target.location.value);
-  console.log(event.target.issue.value);
-}
+
+
+
+NewTicketForm.propTypes = {
+  onNewTicketCreation: PropTypes.func
+};
 
 export default NewTicketForm;
